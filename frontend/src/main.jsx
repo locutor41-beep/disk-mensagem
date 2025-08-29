b92fe70dd255af3090848ecea4e3dd82e6cff4a4
locutor41-beep <locutor41@gmail.com>
2025-08-29 20:15:31 -0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import App from './App'
-import Admin from './Admin'
+// estes arquivos existem na sua pasta src (vi no seu print)
+import App from "./App.jsx";      // site do cliente
+import Admin from "./Admin.jsx";  // painel de administração
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      {/* Site público (clientes) */}
-      <Route path="/*" element={<App />} />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Painel do admin */}
+        <Route path="/admin/*" element={<Admin />} />
 
-      {/* Painel admin */}
-      <Route path="/admin/*" element={<Admin />} />
-    </Routes>
-  </BrowserRouter>
-)
+        {/* Site público (clientes) */}
+        <Route path="/*" element={<App />} />
+
+        {/* Qualquer rota desconhecida volta pra home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
