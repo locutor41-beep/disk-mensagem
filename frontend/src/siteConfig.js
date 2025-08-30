22ce6b1c0d607b53d12ece93620d1db5876acc60
locutor41-beep <locutor41@gmail.com>
2025-08-30 02:38:56 -0200

    Create siteConfig.js

---
+++ frontend/src/siteConfig.js
@@ -0,0 +1,7 @@
+// frontend/src/siteConfig.js
+export default {
+  base_price_cents: 7000,            // 70,00 = 7000 centavos
+  city_name: "Sua Cidade",
+  whats_e164: "5518997053664",       // número no formato E.164 (só dígitos, com DDI)
+  phone_display: "(18) 99705-3664"   // como aparece no botão "Ligar"
+};
