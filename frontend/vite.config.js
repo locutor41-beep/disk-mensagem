3ef1628813cf178384b588136a9c5c38a81f7551
locutor41-beep <locutor41@gmail.com>
2025-08-30 03:59:19 -0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: "es2018"
-  }
+  plugins: [react()]
 });
