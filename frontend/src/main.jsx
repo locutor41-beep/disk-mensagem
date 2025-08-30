d9224b0e3f77969b9e41172b56aee180cf029213
locutor41-beep <locutor41@gmail.com>
2025-08-30 04:00:24 -0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./styles.css"; // Se não tiver styles.css, comente essa linha
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
