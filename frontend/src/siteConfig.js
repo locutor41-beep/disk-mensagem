67ad092cc41deb00edc20bb9762ccb0e4e522d2e
locutor41-beep <locutor41@gmail.com>
2025-08-30 02:50:01 -0200

    Update siteConfig.js

--- frontend/src/siteConfig.js
+++ frontend/src/siteConfig.js
@@ -1,7 +1,9 @@
 // frontend/src/siteConfig.js
+
 export default {
-  base_price_cents: 7000,            // 70,00 = 7000 centavos
-  city_name: "Sua Cidade",
-  whats_e164: "5518997053664",       // número no formato E.164 (só dígitos, com DDI)
-  phone_display: "(18) 99705-3664"   // como aparece no botão "Ligar"
+  appName: "Disk Mensagem",           // Nome do app
+  base_price_cents: 7000,             // 70,00 = 7000 centavos
+  city_name: "Sua Cidade",            // Nome da cidade
+  whats_e164: "5518997053664",        // Número no formato E.164 (só dígitos, com DDI)
+  phone_display: "(18) 99705-3664"    // Como aparece no botão "Ligar"
 };
